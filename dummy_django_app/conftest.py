6d7da6cd54937d61531ef8ac399c65273098434f
irfan87 <nerve2009@yahoo.com>
2022-12-12 23:50:33 +0800

    first commit

---
+++ dummy_django_app/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from dummy_django_app.users.models import User
+from dummy_django_app.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
